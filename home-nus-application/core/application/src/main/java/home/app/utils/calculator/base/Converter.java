e2fc3cd41ca7b5166528a99881f6d086f4877ed0
OlegGromov91 <grom4@mail.ru>
2022-12-10 22:26:12 +0400

    Converter base class

---
+++ home-nus-application/core/application/src/main/java/home/app/utils/calculator/base/Converter.java
@@ -0,0 +1,9 @@
+package home.app.utils.calculator.base;
+
+public interface Converter {
+
+    Long KILOBYTE_MAX_VALUE = 999_999L;
+    Long MEGABYTE_MAX_VALUE = 99_999_999L;
+    Long GIGABYTE_MAX_VALUE = 999_999_999_999L;
+
+}
