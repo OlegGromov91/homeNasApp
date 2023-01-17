0b2c9d3badb9d1eadce637f94b079f426c17f298
OlegGromov91 <grom4@mail.ru>
2023-01-17 22:40:31 +0400

    Рест сервис для бота

---
+++ home-nus-application/ui/ui-adapter/telegram-bot-adapter/src/main/java/home/app/service/rest/RestTelegramBotService.java
@@ -0,0 +1,57 @@
+package home.app.service.rest;
+
+import home.app.exception.RestQbTorrentException;
+import home.app.utils.downloader.DefaultFileDownloader;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+@Service
+public class RestTelegramBotService {
+
+    @Value("${bot.token}")
+    private String botToken;
+    @Value("${bot.file-info}")
+    private String fileInfoUri;
+    @Value("${bot.file-storage}")
+    private String fileStorageUri;
+    @Value("${home-application.qbTorrent.requestTimeout}")
+    private Long requestTimeout;
+
+    @Autowired
+    private WebClient webClient;
+    @Autowired
+    private DefaultFileDownloader defaultFileDownloader;
+
+    public JSONObject getFileInfo(String fileId) {
+        String fileInfoUri = this.fileInfoUri.replace("{token}", botToken)
+                .replace("{fileId}", fileId);
+        try {
+            String response = webClient.get()
+                    .uri(fileInfoUri)
+                    .retrieve()
+                    .bodyToMono(new ParameterizedTypeReference<String>() {
+                    })
+                    .block(Duration.of(requestTimeout, ChronoUnit.MINUTES));
+            return new JSONObject(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RestQbTorrentException("get error when try request to telegram with url " + fileInfoUri + " " + e);
+        }
+
+    }
+
+    public byte[] downloadFileFromTelegram(String filePath) {
+        String storageUri = fileStorageUri.replace("{token}", botToken)
+                .replace("{filePath}", filePath);
+        return defaultFileDownloader.downloadFile(storageUri);
+    }
+
+
+}
